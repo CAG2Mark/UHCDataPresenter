34ebbadf03bac2ebeb6db47e685b9cb7e606fdcc
CAG2Mark <markng989@gmail.com>
2021-05-08 16:58:17 +0800

    Added mod structure

---
+++ src/main/java/me/markng/uhcdatapresenter/UHCDataPresenter.java
@@ -0,0 +1,26 @@
+package me.markng.uhcdatapresenter;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class UHCDataPresenter implements ModInitializer {
+
+    public static Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "uhc-data-presenter";
+    public static final String MOD_NAME = "UHCDataPresenter";
+
+    @Override
+    public void onInitialize() {
+        log(Level.INFO, "Initializing");
+        //TODO: Initializer
+    }
+
+    public static void log(Level level, String message){
+        LOGGER.log(level, "["+MOD_NAME+"] " + message);
+    }
+
+}
